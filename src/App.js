bbe234df0d3d31e90f9a3ea55bacd1237b98603d
konoozgamal <aya476771@gmail.com>
2023-09-30 17:13:05 +0300

    page service

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Services from "./pages/User/Services/Services";
 import Service from "./pages/User/Services/Service_1/Service";
 import ServiceTwo from "./pages/User/Services/Service_2/Service_2";
 import ServiceThree from "./pages/User/Services/Service_3/Service_3";
-// import ServiceF from "./pages/User/Services/Service_4/Service_4";
+// import Ser from "./pages/User/Services/Service_4/Service";
 import ContactUs from "./pages/User/Contact Us/ContactUs";
 import Footer from "./Components/Footer/Footer";
 function App() {
@@ -20,11 +20,10 @@ function App() {
           <Route index path="/" element={<Home />} />
           <Route path="/about" element={<AboutUs/>} />
           <Route path="/contact" element={<ContactUs/>} />
-          <Route path="/services" element={<Services/>}>
-          <Route path="service" element={<Service/>} />
-          <Route path="digital" element={<ServiceTwo/>} />
-          <Route path="business" element={<ServiceThree/>} />
-          </Route>
+          <Route path="/services" element={<Services/>}/>
+          <Route path="/services/service" element={<Service/>} />
+          <Route path="/services/digital" element={<ServiceTwo/>} />
+          <Route path="/services/business" element={<ServiceThree/>} />
         </Routes>
         <Footer /> 
     </div>
--- src/pages/User/Services/Service_4/Service_4.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Service_4() {
-  return (
-    <div>Service_1</div>
-  )
-}
